2999cf601a12e6221e88be1d83e3791821ed9f62
Marilia Santiago <marilia.aline18@gmail.com>
2022-01-11 19:15:43 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/devsuperior/dsmovie/ProjetoCriadoNaSemanaReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCriadoNaSemanaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCriadoNaSemanaReactApplication.class, args);
+	}
+
+}
